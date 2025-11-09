3a0d75326ccd3dae6b03b9ae68751a5db4099a74
wildanjr19 <wildansekolah11@gmail.com>
2025-11-09 22:15:00 +0700

    add Clustering 1

--- Pengantar Data Mining/PDM-R/Clustering/Clustering_1.R
+++ Pengantar Data Mining/PDM-R/Clustering/Clustering_1.R
@@ -1,8 +1,8 @@
 # LIBRARY
 library(dplyr)
 library(car)
-
-
+library(factoextra)
+library(cluster)
 
 # DATA
 data <- read.csv(
@@ -20,49 +20,39 @@ data <- data %>%
 ## ENCODING
 data$status_type <- as.numeric(as.factor(data$status_type))
 
-## DROP MULTIKOLINEARITAS
+## HANDLE MULTIKOLINEARITAS
 ### CEK MULTIKOLINEARITAS
+# pastikan semua varabel numerik
+numeric_data <- data %>% select(where(is.numeric))
 vif_model <- lm(numeric_data[,1] ~ ., data = numeric_data)
 vif(vif_model)
 
-## 
-
-
+### PCA
+# scaling
+scaled_data <- scale(data %>% select(where(is.numeric)))
+pca_result <- prcomp(scaled_data, scale. = FALSE)  # sudah distandarisasi manual
+# cek komponen
+summary(pca_result)
+# lihat komposisi komponen dari setiap variabel
+pca_result$rotation
+# buat data baru dengan komponen yang terpilih
+pca_data <- as.data.frame(pca_result$x[, 1:4])
 
 # KLUSTERING
-## SILHOUTTE
-
 ## ELBOW
+fviz_nbclust(pca_data, kmeans, method = "wss") +
+  labs(title = "Elbow Method untuk Menentukan Jumlah Kluster")
 
+## KMEANS
+set.seed(123)
+kmeans_result <- kmeans(pca_data, centers = 4)
+pca_data$cluster <- as.factor(kmeans_result$cluster)
 
-# KARAKTERISTIK KLUSTER
+## SILHOUTTE
+sil <- silhouette(kmeans_result$cluster, dist(pca_data[, 1:4]))
+fviz_silhouette(sil)
 
-
-
-
-# hapus beberapa kolom yang tidak dibutuhkan
-
-
-
-
-# label encoder
-
-
-# cek multikolinearitas
-
-
-# Pastikan semua variabel numerik
-numeric_data <- data %>% select(where(is.numeric))
-
-# Fit model semu (misalnya regresi terhadap salah satu variabel)
-
-
-
-library(corrplot)
-corrplot(cor(data %>% select(starts_with("num_"))), method = "color")
-
-
-pca_result <- prcomp(data %>% select(where(is.numeric)), scale. = TRUE)
-summary(pca_result)
-
-pca_result$rotation
+fviz_nbclust(pca_data, 
+             kmeans, 
+             method = "silhouette") +
+  labs(title = "Silhouette Method untuk Menentukan Jumlah Kluster Optimal")
